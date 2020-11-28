0fa05057d2d4b981a25712b8f736290f3beb468c
Connor Rose <connorrose.git@gmail.com>
2020-11-27 16:07:48 -0800

    initial hooks setup

--- src/Counter.js
+++ src/Counter.js
@@ -1,78 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-/* ----- HELPERS ----- */
-// setState function can be extracted out for testing
-// Current state & components props are both passed in
-const increment = (state, props) => {
-  const { count } = state;
-  const { max, step } = props;
-  if (count >= max) return;
-  return { count: count + step }
+const getStateFromLocalStore = (key) => {
+  const value = localStorage.getItem(key);
+  if (value !== undefined) {
+    return JSON.parse(value);
+  }
 }
 
-const getStateFromLocalStorage = () => {
-  const storage = localStorage.getItem('counterState');
-  if (storage) return JSON.parse(storage);
-  return { count: 0 };
-}
-
-const updateTitleAndLocalStorage = (state) => {
-  const { count } = state;
-  document.title = `Count: ${count}`;
-  localStorage.setItem('counterState', JSON.stringify(state));
+const storeStateInLocalStorage = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
 }
 
 /* ----- COMPONENT ----- */
-class Counter extends Component {
-  constructor(props) {
-    super(props);
+const Counter = ({ max, step }) => {
+  const [count, setCount] = useState(0);
 
-    this.state = getStateFromLocalStorage();
-    document.title = `Count: ${this.state.count}`
-    
-    this.passStateToCallback = this.passStateToCallback.bind(this);
-    this.updateTitleOnly = this.updateTitleOnly.bind(this);
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
-    this.reset = this.reset.bind(this);
-  }
+  useEffect(() => {
+    const storedCount = getStateFromLocalStore('count');
+    if (storedCount) setCount(storedCount);
+  }, [])
+  
+  useEffect(() => {
+    document.title = `Count: ${count}`
+    storeStateInLocalStorage('count', count)
+  }, [count])
 
-  // Utility for passing state to callbacks
-  passStateToCallback(callback) {
-    return () => callback(this.state);
+  const increment = () => {
+    // updater function does NOT get props
+    if (count < max) setCount(state => state + step);
   }
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
 
-  // Alternative pattern for reusing state callbacks with this
-  updateTitleOnly() {
-    document.title = `Count: ${this.state.count}`
-  }
-
-  // 2nd arg: optional callback to setState
-  // runs after state update & receives no args
-  increment() {
-    this.setState(increment, this.passStateToCallback(updateTitleAndLocalStorage));
-  }
-
-  decrement() {
-    this.setState({ count: this.state.count - 1 }, this.updateTitleOnly);
-  }
-
-  reset() {
-    this.setState({ count: 0 }, this.updateTitleOnly);
-  }
-
-  render() {
-    return (
-      <div className="Counter">
-        <p className="count">{this.state.count}</p>
-        <section className="controls">
-          <button onClick={this.increment}>Increment</button>
-          <button onClick={this.decrement}>Decrement</button>
-          <button onClick={this.reset}>Reset</button>
-        </section>
-      </div>
-    );
-  }
+  return (
+    <div className="Counter">
+      <p className="count">{count}</p>
+      <section className="controls">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </section>
+    </div>
+  );
 }
 
 export default Counter;
